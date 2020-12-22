6f67d93be9d9fa22ad90ba6e63e909bb6e04c22f
Anthony RYCK <anthony.ryck@hotmail.fr>
2020-12-22 10:24:54 +0100

    Ajout du code pour Communication entre composants Part 1. EventCallBack

---
+++ src/BlazorEventCallBack/BlazorEventCallBack/Data/Fan.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorEventCallBack.Data
+{
+	public class Fan
+	{
+		/// <summary>
+		/// Nom du fan
+		/// </summary>
+		public string Nom { get; set; }
+
+		/// <summary>
+		/// Nombre de clique reçu
+		/// </summary>
+		public int NombreDeClickRecu { get; set; }
+
+	}
+}
---
+++ src/BlazorEventCallBack/BlazorEventCallBack/Data/FansService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorEventCallBack.Data
+{
+	public class FansService
+	{
+		/// <summary>
+		/// Collection des fans.
+		/// </summary>
+		public List<Fan> FansCollection { get; set; }
+
+
+		public FansService()
+		{
+			FansCollection = new List<Fan>();
+			FansCollection.Add(new Fan() { Nom = "Fan1", NombreDeClickRecu = 0 });
+			FansCollection.Add(new Fan() { Nom = "Fan2", NombreDeClickRecu = 0 });
+			FansCollection.Add(new Fan() { Nom = "Fan3", NombreDeClickRecu = 0 });
+			FansCollection.Add(new Fan() { Nom = "Fan4", NombreDeClickRecu = 0 });
+			FansCollection.Add(new Fan() { Nom = "Fan5", NombreDeClickRecu = 0 });
+			FansCollection.Add(new Fan() { Nom = "Fan6", NombreDeClickRecu = 0 });
+			FansCollection.Add(new Fan() { Nom = "Fan7", NombreDeClickRecu = 0 });
+		}
+
+
+
+		public void GetClick(string nom)
+		{
+			// Ajout un click qu compteur.
+			FansCollection.Find(x => x.Nom == nom).NombreDeClickRecu += 1;
+		}
+
+	}
+}
