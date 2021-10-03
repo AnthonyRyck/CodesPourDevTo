95b88ffc4f71f66ef5cb563f165168dcdc04ca81
Anthony RYCK <anthony.ryck@hotmail.fr>
2021-10-03 09:50:25 +0200

    Ajout projet pour CosmoDb Gremlin

---
+++ src/EveCosmoGremlin/EveCosmoGremlin/Program.cs
@@ -0,0 +1,48 @@
+﻿using EveCosmoGremlin.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace EveCosmoGremlin
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("##### Lancement de l'application ######");
+			Console.WriteLine("# Appuyer sur une touche pour commencer l'injection.");
+			Console.ReadKey();
+
+			string pathSolarSystem = @"F:\_CodeSource\GitHub\CodesPourDevTo\src\Files\EveOnline\systemSolar.json";
+			string pathJumps = @"F:\_CodeSource\GitHub\CodesPourDevTo\src\Files\EveOnline\Jumps.json";
+
+			Console.WriteLine("Chargement du fichier des systèmes solaires...");
+			string jsonContentSystems = File.ReadAllText(pathSolarSystem);
+			List<SolarSystem> allSolarSystems = JsonSerializer.Deserialize<List<SolarSystem>>(jsonContentSystems);
+			Console.WriteLine($"# Il y a {allSolarSystems.Count} systèmes solaires.");
+
+			Console.WriteLine("Chargement du fichier des jumps...");
+			string jsonContentJumps = File.ReadAllText(pathJumps);
+			List<Jumps> allJumps = JsonSerializer.Deserialize<List<Jumps>>(jsonContentJumps);
+			Console.WriteLine($"# Il y a {allJumps.Count} sauts interstellaires possibles.");
+
+			Console.WriteLine("Fin du chargement des fichiers...");
+			Console.WriteLine("# Début de la création du Graph !");
+			
+			
+			Loader loadData = new Loader();
+
+			//Console.WriteLine("#--> Création des systèmes (Vertex)");
+			//loadData.CreateAllSystems(allSolarSystems).Wait();
+
+
+			Console.WriteLine("#--> Création des liens (Edge)");
+			loadData.CreateEdges(allJumps).Wait();
+
+			Console.WriteLine("##### FIN de l'application ######");
+			Console.ReadKey();
+		}
+	}
+}
