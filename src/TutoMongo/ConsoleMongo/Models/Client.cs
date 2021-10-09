b12ed908fc3611d72bc26ede43be0829069131c6
AnthonyRyck <anthony.ryck@hotmail.fr>
2021-10-09 06:28:39 +0200

    AJout tuto pour MongoDb

---
+++ src/TutoMongo/ConsoleMongo/Models/Client.cs
@@ -0,0 +1,47 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+namespace ConsoleMongo.Models
+{
+    public class Client : MongoId
+    {
+        [BsonElement("Prenom")]
+        public string Prenom { get; set; }
+
+        [BsonElement("Nom")]
+        public string Nom { get; set; }
+
+        [BsonElement("Genre")]
+        public string Genre { get; set; }
+
+        [BsonElement("Age")]
+        public int Age { get; set; }
+
+        [BsonElement("Adresse")]
+        public Adresse Adresse { get; set; }
+
+        [BsonElement("Telephone")]
+        public List<Telephone> Telephone { get; set; }
+    }
+
+    public class Adresse
+    {
+        [BsonElement("Numero")]
+        public string Numero { get; set; }
+
+        [BsonElement("Rue")]
+        public string Rue { get; set; }
+
+        [BsonElement("Ville")]
+        public string Ville { get; set; }
+    }
+
+    public class Telephone
+    {
+        [BsonElement("Type")]
+        public string Type { get; set; }
+
+        [BsonElement("Number")]
+        public string Number { get; set; }
+    }
+}
---
+++ src/TutoMongo/ConsoleMongo/Models/Commande.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMongo.Models
+{
+	public class Commande : MongoId
+	{
+		[BsonElement("ClientId")]
+		public string ClientId { get; set; }
+
+
+	}
+}
