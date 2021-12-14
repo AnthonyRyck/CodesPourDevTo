505484fe8ec24775fadc8462ff88f0cdf89eb45a
Anthony RYCK <anthony.ryck@hotmail.fr>
2021-12-14 05:38:25 +0100

    Ajout ConsoleExtension, modif sur Program.cs

--- src/dotNet6/TutoGrpc/ConsoleClient/Program.cs
+++ src/dotNet6/TutoGrpc/ConsoleClient/Program.cs
@@ -1,16 +1,39 @@
 ﻿using Grpc.Net.Client;
 using ClientGrpc;
 
+"##### Début de l'application de test #####".ToConsoleInfo();
 
-Console.WriteLine("##### Début de l'application de test #####");
-
+":> Connexion au serveur.".ToConsoleInfo();
 var channel = GrpcChannel.ForAddress("https://localhost:7222");
 var client = new Utilisateurs.UtilisateursClient(channel);
 
+":> Récupération du nombre d'utilisateur".ToConsoleInfo();
+"Appuyer sur une touche pour continuer".ToConsoleInfo();
+ReadKey();
 Empty empty = new Empty();
 CountUser countUser = await client.GetCountUserAsync(empty);
+$"Nombre d'utilisateurs : {countUser.NombreUser}".ToConsoleResult();
+WriteLine();
 
-Console.WriteLine($"Nombre d'utilisateurs : {countUser.NombreUser}");
+":> Récupération de tous les clients".ToConsoleInfo();
+"Appuyer sur une touche pour continuer".ToConsoleInfo();
+ReadKey();
+var allClients = await client.GetAllAsync(empty);
+"Récupération OK".ToConsoleResult();
+":> Comparaison entre le nombre de client reçu et toute la liste.".ToConsoleInfo();
+$"--> Compteur à {countUser.NombreUser} et nombre dans la liste : {allClients.AllUsers.Count}".ToConsoleInfo();
+WriteLine();
 
-Console.WriteLine("##### Fin de l'application de test #####");
-Console.ReadKey();
+":> Récupération d'un utilisateur avec son ID".ToConsoleInfo();
+"Appuyer sur une touche pour continuer".ToConsoleInfo();
+ReadKey();
+string idUser = "61b6c9ce2ad8b28e03238b33";
+var user = await client.GetUserByIdAsync(new UserIdRequest(){ IdUser = idUser });
+"Récupération de l'utilisateur OK".ToConsoleResult();
+"Appuyer sur une touche pour voir le résultat".ToConsoleInfo();
+ReadKey();
+user.ToString().ToConsoleInfo();
+WriteLine();
+
+"##### Fin de l'application de test #####".ToConsoleInfo();
+ReadKey();
