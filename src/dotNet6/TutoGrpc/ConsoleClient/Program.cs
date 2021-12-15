1090dd93981442ab366956413bdca1d34cd5f200
AnthonyRyck <anthony.ryck@hotmail.fr>
2021-12-15 14:33:10 +0100

    Ajout dockerfile

--- src/dotNet6/TutoGrpc/ConsoleClient/Program.cs
+++ src/dotNet6/TutoGrpc/ConsoleClient/Program.cs
@@ -3,37 +3,38 @@ using ClientGrpc;
 using System.Diagnostics;
 using System.Text.Json;
 
-const string BASE_ADDRESS = "https://localhost:7222";
+//const string BASE_ADDRESS = "https://localhost:7222";
+string BASE_ADDRESS = "https://grpc-noauth.ctrl-alt-suppr.dev/";
 
 "##### Début de l'application de test #####".ToConsoleInfo();
 "Appuyer sur une touche pour continuer".ToConsoleInfo();
 ReadKey();
 
-":> Connexion au serveur.".ToConsoleInfo();
-var channel = GrpcChannel.ForAddress(BASE_ADDRESS, new GrpcChannelOptions
-{
-    MaxReceiveMessageSize = null
-});
-var client = new Utilisateurs.UtilisateursClient(channel);
-
-":> Récupération du nombre d'utilisateur".ToConsoleInfo();
-"Appuyer sur une touche pour continuer".ToConsoleInfo();
-ReadKey();
-Empty empty = new Empty();
-CountUser countUser = await client.GetCountUserAsync(empty);
-$"Nombre d'utilisateurs : {countUser.NombreUser}".ToConsoleResult();
-WriteLine();
-
-":> Récupération d'un utilisateur avec son ID".ToConsoleInfo();
-"Appuyer sur une touche pour continuer".ToConsoleInfo();
-ReadKey();
-string idUser = "61b6c9ce2ad8b28e03238b33";
-var user = await client.GetUserByIdAsync(new UserIdRequest() { IdUser = idUser });
-"Récupération de l'utilisateur OK".ToConsoleResult();
-"Appuyer sur une touche pour voir le résultat".ToConsoleInfo();
-ReadKey();
-user.ToString().ToConsoleInfo();
-WriteLine();
+//":> Connexion au serveur.".ToConsoleInfo();
+//var channel = GrpcChannel.ForAddress(BASE_ADDRESS, new GrpcChannelOptions
+//{
+//    MaxReceiveMessageSize = null
+//});
+//var client = new Utilisateurs.UtilisateursClient(channel);
+//
+//":> Récupération du nombre d'utilisateur".ToConsoleInfo();
+//"Appuyer sur une touche pour continuer".ToConsoleInfo();
+//ReadKey();
+//Empty empty = new Empty();
+//CountUser countUser = await client.GetCountUserAsync(empty);
+//$"Nombre d'utilisateurs : {countUser.NombreUser}".ToConsoleResult();
+//WriteLine();
+//
+//":> Récupération d'un utilisateur avec son ID".ToConsoleInfo();
+//"Appuyer sur une touche pour continuer".ToConsoleInfo();
+//ReadKey();
+//string idUser = "61b6c9ce2ad8b28e03238b33";
+//var user = await client.GetUserByIdAsync(new UserIdRequest() { IdUser = idUser });
+//"Récupération de l'utilisateur OK".ToConsoleResult();
+//"Appuyer sur une touche pour voir le résultat".ToConsoleInfo();
+//ReadKey();
+//user.ToString().ToConsoleInfo();
+//WriteLine();
 
 var chrono = new Stopwatch();
 
@@ -47,28 +48,29 @@ HttpResponseMessage resultController = await httpClient.GetAsync(BASE_ADDRESS + 
 using(Stream reponse = await resultController.Content.ReadAsStreamAsync())
 {
     var tousLesUtilisateurs = JsonSerializer.DeserializeAsync<List<User>>(reponse);
+    $"Il y a {tousLesUtilisateurs.Result.Count} utilisateurs".ToConsoleResult();
 }
 
 chrono.Stop();
-var timeController = chrono.ElapsedMilliseconds;
-$"Récupération OK en {timeController} millisecondes".ToConsoleResult();
-
-":> Récupération de tous les clients via gRPC".ToConsoleInfo();
-"Appuyer sur une touche pour continuer".ToConsoleInfo();
-ReadKey();
-
-chrono.Restart();
-var allClients = await client.GetAllAsync(empty);
-chrono.Stop();
-var timeGrpc = chrono.ElapsedMilliseconds;
-
-$"Récupération OK en {timeGrpc} millisecondes".ToConsoleResult();
-":> Comparaison entre le nombre de client reçu et toute la liste.".ToConsoleInfo();
-$"--> Compteur à {countUser.NombreUser} et nombre dans la liste : {allClients.AllUsers.Count}".ToConsoleInfo();
-WriteLine();
-
-$"--> DIFFERENCE entre le temps gRPC et Controller : {timeGrpc} - {timeController} = {timeGrpc - timeController} milliseconds".ToConsoleInfo();
-WriteLine();
+//var timeController = chrono.ElapsedMilliseconds;
+//$"Récupération OK en {timeController} millisecondes".ToConsoleResult();
+//
+//":> Récupération de tous les clients via gRPC".ToConsoleInfo();
+//"Appuyer sur une touche pour continuer".ToConsoleInfo();
+//ReadKey();
+//
+//chrono.Restart();
+//var allClients = await client.GetAllAsync(empty);
+//chrono.Stop();
+//var timeGrpc = chrono.ElapsedMilliseconds;
+//
+//$"Récupération OK en {timeGrpc} millisecondes".ToConsoleResult();
+//":> Comparaison entre le nombre de client reçu et toute la liste.".ToConsoleInfo();
+//$"--> Compteur à {countUser.NombreUser} et nombre dans la liste : {allClients.AllUsers.Count}".ToConsoleInfo();
+//WriteLine();
+//
+//$"--> DIFFERENCE entre le temps gRPC et Controller : {timeGrpc} - {timeController} = {timeGrpc - timeController} milliseconds".ToConsoleInfo();
+//WriteLine();
 
 "##### Fin de l'application de test #####".ToConsoleInfo();
 ReadKey();
--- src/dotNet6/TutoGrpc/ServerGrpc/Program.cs
+++ src/dotNet6/TutoGrpc/ServerGrpc/Program.cs
@@ -1,28 +1,38 @@
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using ServerGrpc.Services;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.ConfigureKestrel((context, options) =>
+{
+    options.Listen(IPAddress.Any, 5001, listenOptions =>
+    {
+        listenOptions.Protocols = HttpProtocols.Http1AndHttp2AndHttp3;
+    });
+});
 
 builder.Services.AddSingleton<IDataService, DataService>();
 
 // Ajout du service Grpc
 builder.Services.AddGrpc(options =>
 {
+    // Pour l'envoie de message sans limite de taille
     options.MaxSendMessageSize = null;
 });
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
+app.UseRouting();
 
 app.MapGrpcService<UtilisateursService>();
-app.MapGet("/", () => "C'est la loose...");
+app.MapGet("/", () => "C'est pour l'article sur gRPC sur www.ctrl-alt-suppr.dev");
 
 // Pour le controler
-app.UseRouting();
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllers();
 });
-
 
 app.Run();
