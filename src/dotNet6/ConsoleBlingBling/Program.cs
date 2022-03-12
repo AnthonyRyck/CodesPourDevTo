d802af5bf9636222dbc5c24a8e99371da180c278
AnthonyRyck <anthony.ryck@hotmail.fr>
2022-03-12 08:57:01 +0100

    Change menu

--- src/dotNet6/ConsoleBlingBling/Program.cs
+++ src/dotNet6/ConsoleBlingBling/Program.cs
@@ -19,18 +19,21 @@ List<PostWordPress> posts = new List<PostWordPress>();
 await AnsiConsole.Status()
     		.StartAsync("Récupération des 10 derniers post...", async ctx => 
     		{
+				// Change le "waiting".
+				ctx.Spinner(Spinner.Known.Aesthetic);
+
 				using(var client = new HttpClient())
 				{
 					string url = @"https://www.ctrl-alt-suppr.dev/wp-json/wp/v2/posts?per_page=10";
 					var streamPosts = await client.GetStreamAsync(url);
 					posts = await JsonSerializer.DeserializeAsync<List<PostWordPress>>(streamPosts);
 				}
-				// Si ça va trop vite pour la récupération
+				// Pour avoir le temps de voir le "waiting".
 				await Task.Delay(5000);
 				AnsiConsole.MarkupLine("Terminé");
     		});
 
-string cmdQuit = "/quit";
+string cmdQuit = "quit";
 string selection = string.Empty;
 
 ShowPosts();
@@ -41,23 +44,24 @@ while(selection != cmdQuit)
 
 	switch (selection)
 	{
-		case "/quit":
+		case "quit":
+			AnsiConsole.MarkupLine("See you soon " + Emoji.Known.OkHand);
 			Environment.Exit(0);
 			break;
-		case "/posts":
+		case "posts":
 			ShowPosts();
 			break;
-		case "/post1":
-		case "/post2":
-		case "/post3":
-		case "/post4":
-		case "/post5":
-		case "/post6":
-		case "/post7":
-		case "/post8":
-		case "/post9":
-		case "/post10":
-			int index = int.Parse(selection.Replace("/post", string.Empty));
+		case "1":
+		case "2":
+		case "3":
+		case "4":
+		case "5":
+		case "6":
+		case "7":
+		case "8":
+		case "9":
+		case "10":
+			int index = int.Parse(selection);
 			string urlPost = posts[index - 1].link;
 			AnsiConsole.MarkupLine("[green]Bonne lecture[/] :grinning_face_with_big_eyes:");
 
@@ -78,8 +82,8 @@ void ShowPosts()
 	// Affichage des posts.
 	for (var i = 0; i < posts.Count; i++)
 	{
-		AnsiConsole.MarkupLine($"/post{i + 1} - {posts[i].title.rendered}");
+		AnsiConsole.MarkupLine($"[purple]{i + 1}[/] - {posts[i].title.rendered}");
 	}
-	AnsiConsole.MarkupLine("/posts : pour réafficher la liste des articles.");
-	AnsiConsole.MarkupLine("/quit : pour quitter.");
+	AnsiConsole.MarkupLine("[green]posts[/] : pour réafficher la liste des articles.");
+	AnsiConsole.MarkupLine("[red]quit[/] : pour quitter.");
 }
